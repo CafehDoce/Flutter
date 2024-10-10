edca518a34ebd5333597c6881eb7ea347d1d31ec
Isabella F. Ortiz <141460524+CafehDoce@users.noreply.github.com>
2024-10-09 22:20:27 -0300

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -8,101 +8,34 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Lista To-Do',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: TodoList(),
+      home: Scaffold(
+        appBar: AppBar(title: Text('E-commerce')),
+        body: Center(
+          child: CustomButton(
+            onPressed: () {
+              print('Botão pressionado!');
+            },
+            text: 'Clique aqui',
+          ),
+        ),
+      ),
     );
   }
 }
 
-class TodoList extends StatefulWidget {
-  @override
-  _TodoListState createState() => _TodoListState();
-}
+class CustomButton extends StatelessWidget {
+  final VoidCallback onPressed;
+  final String text;
 
-class _TodoListState extends State<TodoList> {
-  final List<Map<String, dynamic>> _tasks = [];
-  final TextEditingController _controller = TextEditingController();
-
-  void _addTask() {
-    if (_controller.text.isNotEmpty) {
-      setState(() {
-        _tasks.add({'title': _controller.text, 'done': false});
-        _controller.clear();
-      });
-    }
-  }
-
-  void _toggleTask(int index) {
-    setState(() {
-      _tasks[index]['done'] = !_tasks[index]['done'];
-    });
-  }
-
-  void _removeTask(int index) {
-    setState(() {
-      _tasks.removeAt(index);
-    });
-  }
+  const CustomButton({
+    required this.onPressed,
+    required this.text,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Lista To-Do'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            TextField(
-              controller: _controller,
-              decoration: InputDecoration(
-                labelText: 'Adicionar nova tarefa',
-                suffixIcon: IconButton(
-                  icon: Icon(Icons.add),
-                  onPressed: _addTask,
-                ),
-              ),
-            ),
-            SizedBox(height: 16.0),
-            Expanded(
-              child: ListView.builder(
-                itemCount: _tasks.length,
-                itemBuilder: (context, index) {
-                  return Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 4.0),
-                    child: Row(
-                      children: [
-                        Checkbox(
-                          value: _tasks[index]['done'],
-                          onChanged: (value) {
-                            _toggleTask(index);
-                          },
-                        ),
-                        Expanded(
-                          child: Text(
-                            _tasks[index]['title'],
-                            style: TextStyle(
-                              decoration: _tasks[index]['done']
-                                  ? TextDecoration.lineThrough
-                                  : null,
-                            ),
-                          ),
-                        ),
-                        IconButton(
-                          icon: Icon(Icons.delete),
-                          onPressed: () => _removeTask(index),
-                        ),
-                      ],
-                    ),
-                  );
-                },
-              ),
-            ),
-          ],
-        ),
-      ),
+    return GestureDetector(
+      onTap: onPressed,
     );
   }
 }
